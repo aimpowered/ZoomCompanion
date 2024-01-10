1090be1b6b8c9e14fd986462eb2c3d3b84a35729
Kexin Feng <kexin@tamu.edu>
2024-01-10 05:26:34 -0600

    use youtube video instead of local video

--- app/main/Affirmation.tsx
+++ app/main/Affirmation.tsx
@@ -4,8 +4,8 @@ import Footer from '../footer';
 
 function Home() {
 
-  const video1Path = 'affirmation_videos/1_Minute_Breathing_Exercise_Mini_Meditation_Simple_Breathing_Exercise_One_Minute_Breathe_Bubble.mp4'; // Replace with the actual path to your video1
-  const video2Path = 'affirmation_videos/Standing_Upper_Body_Stretches.mp4';
+  const youtube1ID = 'SD6dPo98dWw'
+  const youtube2ID = '90mqR3A9Pno'
 
   return (
     <div className="bg-white w-screen h-screen">
@@ -16,18 +16,22 @@ function Home() {
       <div style={{ marginLeft: '20px', marginRight: '20px' }}>
         <h2 style={{ fontSize: '30px', fontWeight: 'bold', display: 'inline-block' }}>Mindfulness</h2>
           <div className="flex flex-col items-center mt-4">
-            <div style={{ width: '100%', marginBottom: '20px' }}>
-              <video controls width="100%" height="100%">
-                <source src={video1Path} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+            <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%', marginBottom: '20px' }}>
+              <iframe
+                className='video'
+                title='Youtube player'
+                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                src={`https://www.youtube.com/embed/${youtube1ID}`}
+              ></iframe>
             </div>
 
-            <div style={{ width: '100%' }}>
-              <video controls width="100%" height="100%">
-                <source src={video2Path} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+            <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%' }}>
+              <iframe
+                className='video'
+                title='Youtube player'
+                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                src={`https://www.youtube.com/embed/${youtube2ID}`}
+              ></iframe>
             </div>
           </div>
       </div>
