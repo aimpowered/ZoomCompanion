91d762ae9b7ef8e6f27296c4b80ceb02d67b6e25
Kexin Feng <kexin@tamu.edu>
2024-01-14 18:00:11 -0600

    check if affirmation button exists in the page

--- __tests__/affirmation.test.tsx
+++ __tests__/affirmation.test.tsx
@@ -8,9 +8,25 @@ jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
 describe('Page', () => {
   it('renders a heading', () => {
     render(<Page />)
-
     const heading = screen.getByRole('heading', { level: 1 })
-
     expect(heading).toBeInTheDocument()
   })
+
+  it('affirmation buttons existed in this page', () => {
+    render(<Page />)
+    const buttonTexts = [
+      'I have an important voice',
+      'Say what I want to say, whatever happens will help me grow',
+      'I can take up space',
+      'Feel the tension and proceed',
+      'I have the right to stutter',
+    ];
+
+    buttonTexts.forEach((text) => {
+      const button = screen.getByText(text);
+      expect(button).toBeInTheDocument();
+    });
+  })
+
+
 })
