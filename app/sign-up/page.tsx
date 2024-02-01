ea1816c6c69268947b8c939767ca1525f465e8de
Kexin Feng <kexin@tamu.edu>
2024-02-01 02:26:24 -0600

    sign up button

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -74,14 +74,18 @@ return (
                             required
                         />
                     </div>
-                    <button
-                        className="w-full bg-yellow-500 text-black px-4 py-2 rounded-md font-bold hover:bg-blue-600 transition-colors"
-                        type="submit"
-                        disabled={busy}
-                        style={{ opacity: busy ? 0.5 : 1 }}
-                    >
-                        Sign Up
-                    </button>
+                    <div className="flex justify-center items-center mt-8">
+                        <button
+                            className="w-full text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors mt-8"
+                            type="submit"
+                            disabled={busy}
+                            style={{ opacity: busy ? 0.5 : 1 , maxWidth: '100px', backgroundColor: '#d68071'}}
+                        >
+                            Sign Up
+                        </button>
+                    </div>
+
+                    
                 </form>
             </div>
         </div>
