8b53ba09d0ce6dd578af803696b1addf87c86b29
Kexin Feng <kexin@tamu.edu>
2024-02-05 17:57:11 -0600

    wrap the display of mindfulness videos in one single function

--- app/main/Mindfulness.tsx
+++ app/main/Mindfulness.tsx
@@ -1,33 +1,35 @@
 "use client";
 
+interface VideoPlayerProps {
+  videoId: string;
+  testId: string;
+}
+
+const generateVideoPlayer = ({ videoId, testId }: VideoPlayerProps) => (
+  <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%', marginBottom: '20px' }}>
+    <iframe
+      className='video'
+      title='Youtube player'
+      data-testid={testId}
+      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+      src={`https://www.youtube.com/embed/${videoId}`}
+    ></iframe>
+  </div>
+);
+
 function Mindfullness() {
 
   const youtube1ID = 'SD6dPo98dWw'
   const youtube2ID = '90mqR3A9Pno'
+  const youtube1TestId = 'youtube1-iframe';
+  const youtube2TestId = 'youtube2-iframe';
 
   return (
       <div style={{ marginLeft: '20px', marginRight: '20px' }}>
         <h2 style={{ fontSize: '30px', fontWeight: 'bold', display: 'inline-block' }}>Mindfulness</h2>
           <div className="flex flex-col items-center mt-4">
-            <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%', marginBottom: '20px' }}>
-              <iframe
-                className='video'
-                title='Youtube player'
-                data-testid="youtube1-iframe"
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                src={`https://www.youtube.com/embed/${youtube1ID}`}
-              ></iframe>
-            </div>
-
-            <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%' }}>
-              <iframe
-                className='video'
-                title='Youtube player'
-                data-testid="youtube2-iframe"
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                src={`https://www.youtube.com/embed/${youtube2ID}`}
-              ></iframe>
-            </div>
+            {generateVideoPlayer({ videoId: youtube1ID, testId: youtube1TestId })}
+            {generateVideoPlayer({ videoId: youtube2ID, testId: youtube2TestId })}
           </div>
       </div>
   );
