d2911be858568b8a27ca9f50bc4775bf2ebcc947
Kexin Feng <kexin@tamu.edu>
2024-02-05 22:36:05 -0600

    revise the test for mindfulness, use aria label for videos

--- app/main/Mindfulness.tsx
+++ app/main/Mindfulness.tsx
@@ -2,15 +2,15 @@
 
 interface VideoPlayerProps {
   videoId: string;
-  testId: string;
+  Label: string;
 }
 
-const generateVideoPlayer = ({ videoId, testId }: VideoPlayerProps) => (
+const generateVideoPlayer = ({ videoId, Label }: VideoPlayerProps) => (
   <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%', marginBottom: '20px' }}>
     <iframe
       className='video'
       title='Youtube player'
-      data-testid={testId}
+      aria-label={Label}
       style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
       src={`https://www.youtube.com/embed/${videoId}`}
     ></iframe>
@@ -21,15 +21,15 @@ function Mindfullness() {
 
   const youtube1ID = 'SD6dPo98dWw'
   const youtube2ID = '90mqR3A9Pno'
-  const youtube1TestId = 'youtube1-iframe';
-  const youtube2TestId = 'youtube2-iframe';
+  const youtube1Label = 'youtube-video-1';
+  const youtube2Label = 'youtube-video-2';
 
   return (
       <div style={{ marginLeft: '20px', marginRight: '20px' }}>
         <h2 style={{ fontSize: '30px', fontWeight: 'bold', display: 'inline-block' }}>Mindfulness</h2>
           <div className="flex flex-col items-center mt-4">
-            {generateVideoPlayer({ videoId: youtube1ID, testId: youtube1TestId })}
-            {generateVideoPlayer({ videoId: youtube2ID, testId: youtube2TestId })}
+            {generateVideoPlayer({ videoId: youtube1ID, Label: youtube1Label })}
+            {generateVideoPlayer({ videoId: youtube2ID, Label: youtube2Label })}
           </div>
       </div>
   );
