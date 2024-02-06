a0ea3bee9966bfb86bdbd05185e4aaf1bce322d8
Kexin Feng <kexin@tamu.edu>
2024-02-05 22:36:05 -0600

    revise the test for mindfulness, use aria label for videos

--- __tests__/mindfulness.test.tsx
+++ __tests__/mindfulness.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import Page from '../app/mindfulness/page'
+import Page from '../app/main/mindfulness'
+
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
 jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
@@ -9,28 +10,24 @@ describe('Page', () => {
   it('renders a heading and two videos', () => {
     render(<Page />)
 
-    const heading = screen.getByRole('heading', { level: 1 })
 
-    expect(heading).toBeInTheDocument()
-
-    const video1 = screen.getByTestId('youtube1-iframe');
+    const video1 = screen.getByLabelText('youtube-video-1');
     expect(video1).toBeInTheDocument();
-
-    const video2 = screen.getByTestId('youtube2-iframe');
+    
+    const video2 = screen.getByLabelText('youtube-video-2');
     expect(video2).toBeInTheDocument();
-
   })
 
   it('renders two correct videos', () => {
     render(<Page />)
 
-    const youtube1ID = 'SD6dPo98dWw';
-    const youtube2ID = '90mqR3A9Pno';
+    const youtube1ID = 'SD6dPo98dWw'
+    const youtube2ID = '90mqR3A9Pno'
 
-    const video1 = screen.getByTestId('youtube1-iframe');
+    const video1 = screen.getByLabelText('youtube-video-1');
     expect(video1).toHaveAttribute('src', expect.stringContaining(youtube1ID));
 
-    const video2 = screen.getByTestId('youtube2-iframe');
+    const video2 = screen.getByLabelText('youtube-video-2');
     expect(video2).toHaveAttribute('src', expect.stringContaining(youtube2ID));
 
 
