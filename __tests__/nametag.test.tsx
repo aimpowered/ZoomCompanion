8c86940dbf36e066702497fd45b491c3819e0c39
Kexin Feng <kexin@tamu.edu>
2024-03-12 03:06:01 -0500

    Load the whole page instead of only NameTag

--- __tests__/nametag.test.tsx
+++ __tests__/nametag.test.tsx
@@ -2,31 +2,34 @@ import '@testing-library/jest-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 
-import NameTag from '../app/main/NameTag'
+// Uncomment the code below if willing to only test the NamgeTag part
+// import NameTag from '../app/main/NameTag'
+// const currentNameTag = ['', '', '', ''];
+// const nameTagStatus = false;
+// const setCurrentNameTag = jest.fn();
+// const setNameTagStatus = jest.fn();
+// const selectedWaveHand = null;
+// const waveHands = [];
 
-const currentNameTag = ['', '', '', ''];
-const nameTagStatus = false;
-const setCurrentNameTag = jest.fn();
-const setNameTagStatus = jest.fn();
-const selectedWaveHand = null;
-const waveHands = [];
-
+import Page from '../app/main/page'
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
 jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
 
 describe('Page', () => {
   it('renders the heading and input fields', () => {
-    render(
-      <NameTag
-        currentNameTag={currentNameTag}
-        nameTagStatus={nameTagStatus}
-        setCurrentNameTag={setCurrentNameTag}
-        setNameTagStatus={setNameTagStatus}
-        selectedWaveHand={selectedWaveHand}
-        waveHands={waveHands}
-      />
-    );
+    // render(
+    //   <NameTag
+    //     currentNameTag={currentNameTag}
+    //     nameTagStatus={nameTagStatus}
+    //     setCurrentNameTag={setCurrentNameTag}
+    //     setNameTagStatus={setNameTagStatus}
+    //     selectedWaveHand={selectedWaveHand}
+    //     waveHands={waveHands}
+    //   />
+    // );
+
+    render(<Page />)
     expect(screen.getByText('Full Name')).toBeInTheDocument();
     expect(screen.getByText('Preferred Name')).toBeInTheDocument();
     expect(screen.getAllByText('Select Pronouns')[0]).toBeInTheDocument();
@@ -34,16 +37,18 @@ describe('Page', () => {
   })
 
   it('toggles the nametag display on switch change', async () => {
-    render(
-      <NameTag
-        currentNameTag={currentNameTag}
-        nameTagStatus={nameTagStatus}
-        setCurrentNameTag={setCurrentNameTag}
-        setNameTagStatus={setNameTagStatus}
-        selectedWaveHand={selectedWaveHand}
-        waveHands={waveHands}
-      />
-    );
+    // render(
+    //   <NameTag
+    //     currentNameTag={currentNameTag}
+    //     nameTagStatus={nameTagStatus}
+    //     setCurrentNameTag={setCurrentNameTag}
+    //     setNameTagStatus={setNameTagStatus}
+    //     selectedWaveHand={selectedWaveHand}
+    //     waveHands={waveHands}
+    //   />
+    // );
+
+    render(<Page />)
 
     let switchInput = screen.getByRole('switch')
     expect(switchInput).not.toBeChecked();
