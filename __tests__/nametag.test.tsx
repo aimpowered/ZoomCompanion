16ec7f703b478d94f75697dbb25cad7339c0f1a6
Kexin Feng <kexin@tamu.edu>
2024-03-12 14:37:25 -0500

    remove extra comments

--- __tests__/nametag.test.tsx
+++ __tests__/nametag.test.tsx
@@ -2,15 +2,6 @@ import '@testing-library/jest-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 
-// Uncomment the code below if willing to only test the NamgeTag part
-// import NameTag from '../app/main/NameTag'
-// const currentNameTag = ['', '', '', ''];
-// const nameTagStatus = false;
-// const setCurrentNameTag = jest.fn();
-// const setNameTagStatus = jest.fn();
-// const selectedWaveHand = null;
-// const waveHands = [];
-
 import Page from '../app/main/page'
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
@@ -18,16 +9,6 @@ jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
 
 describe('Page', () => {
   it('renders the heading and input fields', () => {
-    // render(
-    //   <NameTag
-    //     currentNameTag={currentNameTag}
-    //     nameTagStatus={nameTagStatus}
-    //     setCurrentNameTag={setCurrentNameTag}
-    //     setNameTagStatus={setNameTagStatus}
-    //     selectedWaveHand={selectedWaveHand}
-    //     waveHands={waveHands}
-    //   />
-    // );
 
     render(<Page />)
     expect(screen.getByText('Full Name')).toBeInTheDocument();
@@ -37,16 +18,6 @@ describe('Page', () => {
   })
 
   it('toggles the nametag display on switch change', async () => {
-    // render(
-    //   <NameTag
-    //     currentNameTag={currentNameTag}
-    //     nameTagStatus={nameTagStatus}
-    //     setCurrentNameTag={setCurrentNameTag}
-    //     setNameTagStatus={setNameTagStatus}
-    //     selectedWaveHand={selectedWaveHand}
-    //     waveHands={waveHands}
-    //   />
-    // );
 
     render(<Page />)
 
