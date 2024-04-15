92fc5e28254433a5747331049e960793afb07fa8
Kexin Feng <kexin@tamu.edu>
2024-04-15 02:25:26 -0500

    now click the button on the home page in testing

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import Page from '../app/main/page'
+import { debug } from 'jest-preview';
+// import ZoomApiWrapper from '../lib/fakezoomapi';
+
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
 jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
@@ -8,6 +11,18 @@ jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
 describe('Load page', () => {
   
   it('renders Affirmation component', () => {
-  	// render(<Page />);
+    render(<Page />);
+
+    act(() => {
+      const firstButtonListItem = document.querySelector('.wave-hand-button');
+
+      if (firstButtonListItem) {
+        firstButtonListItem.click();
+        // expect(ZoomApiWrapper.setVirtualForeground).toHaveBeenCalled();
+      } else {
+        console.error('No element with class tab-list-item found.');
+      }
+    });
+
   });
 });
