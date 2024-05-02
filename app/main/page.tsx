2c1ef60e0c41d9202c6511c5e3585925d5edee69
Brennan Jones <brennandgj@gmail.com>
2024-05-02 11:34:05 -0700

    Update app/main/page.tsx
    Co-authored-by: Ben Lickly <blickly@users.noreply.github.com>

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -14,7 +14,6 @@ import { HandWaveBadge, DrawBadgeApi } from "@/lib/draw_badge_api";
 import { createFromConfig, ZoomApiWrapper } from "@/lib/zoomapi";
 import { ConfigOptions }  from "@zoom/appssdk";
 import { fetchNametagFromDB, updateNameTagInDB } from '@/lib/nametag_db';
-// import { getSession } from 'next-auth/react';
 
 const zoomConfigOptions: ConfigOptions = {
   capabilities: [
