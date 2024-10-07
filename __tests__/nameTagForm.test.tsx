77cb530b4d8a113ab1d1dbe60b19383961a5bfff
charansr <charan.sridhar26@gmail.com>
2024-10-07 02:03:57 -0700

    ran prettier

--- __tests__/nameTagForm.test.tsx
+++ __tests__/nameTagForm.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen, waitFor} from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 
 import { NameTagForm } from "@/components/NameTagForm";
@@ -58,7 +58,7 @@ describe("NameTagForm", () => {
           preferredName: expect.anything(),
           pronouns: expect.anything(),
           disclosure: expect.anything(),
-        })
+        }),
       );
     });
   });
